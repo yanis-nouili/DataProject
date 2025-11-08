daf390a4ebcdc7d9dbeb8d17a6db26a21571691f
yacine.oualiken <yacine.oualiken@edu.esiee.fr>
2025-11-08 18:13:51 +0100

    Update clean_data.py

--- src/utils/clean_data.py
+++ src/utils/clean_data.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import os
 
+RAW_FILE = "data/raw/etat-du-trafic-en-temps-reel.csv"
+CLEAN_FILE = "data/cleaned/etat_du_trafic_clean.csv"
+
+def clean():
+    df = pd.read_csv(RAW_FILE, sep=';')
+
+    df['datetime'] = pd.to_datetime(df['datetime'], errors='coerce')
+
+    colonnes_utiles = [
+        'datetime', 'averageVehicleSpeed', 'travelTime', 'trafficStatus',
+        'Geo Point', 'vitesse_maxi', 'denomination', 'hierarchie_dv'
+    ]
+    df = df[colonnes_utiles]
+
+    df.to_csv(CLEAN_FILE, sep=';', index=False)
+    print("✅ Données nettoyées enregistrées dans", CLEAN_FILE)
+
+if __name__ == "__main__":
+    clean()
