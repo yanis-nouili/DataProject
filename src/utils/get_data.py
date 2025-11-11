26d0335c08bebbe32d3600626fc996fa8df6f5a6
yacine.oualiken <yacine.oualiken@edu.esiee.fr>
2025-11-11 23:38:32 +0100

    Update get_data.py

--- src/utils/get_data.py
+++ src/utils/get_data.py
@@ -1,1 +1,37 @@
+import requests
+from pathlib import Path
 
+def get_data():
+    """
+    Télécharge le fichier CSV brut contenant les données de trafic de Rennes.
+    Le fichier est sauvegardé dans `data/raw/etat-du-trafic-en-temps-reel.csv`
+
+    1/ Définir le chemin de sauvegarde du fichier.
+    2/ Vérifier si le fichier existe déjà.
+    3/ Télécharger le fichier depuis Open Data Rennes si nécessaire.
+    """
+
+    # 1/ On commence par définir le chemin de destination du fichier
+    raw_path = Path("data/raw/etat-du-trafic-en-temps-reel.csv")
+    raw_path.parent.mkdir(parents=True, exist_ok=True)  # création dossier si inexistant
+
+    # 2/ Puis on vérifier si le fichier existe déjà, si oui on arrête la fonction
+    if raw_path.exists():
+        print(f"Fichier déjà présent : {raw_path}")
+        return  
+
+    # 3/ Sinon, on télécharge le fichier depuis le site "Open Data Rennes"
+    url = "https://data.explore.rennesmetropole.fr/api/explore/v2.1/catalog/datasets/etat-du-trafic-en-temps-reel/exports/csv"
+    print("Téléchargement du fichier depuis Open Data Rennes")
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        raw_path.write_bytes(response.content)  # sauvegarde du fichier
+        print(f"Fichier téléchargé et sauvegardé : {raw_path}")
+    else:
+        print(f"Erreur lors du téléchargement. Status code : {response.status_code}")
+
+
+# Point d'entrée pour exécuter directement le script
+if __name__ == "__main__":
+    get_data()
